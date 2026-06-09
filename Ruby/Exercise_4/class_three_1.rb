726d87cd248af4f0e621519f64152ce3a07e29d8
DarioJr02 <dariocmia@gmail.com>
2026-06-09 10:55:24 +0200

    Excercise_4: Trying class inspections

--- Ruby/Exercise_4/class_three_1.rb
+++ Ruby/Exercise_4/class_three_1.rb
@@ -2,3 +2,22 @@
 
 # This file is to practice how class Tree works in Ruby.
 
+class One
+end
+
+class Two < One
+end
+
+class Three < Two
+end
+
+class Four < Three
+end
+
+class Five < Four
+end
+
+ob = Five.new
+tree = ob.class
+
+puts(ob.methods)
