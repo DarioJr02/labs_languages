c332baa3048ed1987ec26ea680f4b06b7112e9dd
DarioJr02 <dariocmia@gmail.com>
2026-06-10 11:51:22 +0200

    String concatenation

--- Ruby/Exercise_5/back_quotes.rb
+++ Ruby/Exercise_5/back_quotes.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# #frozen_string_literal: true
 
 # This file is to practice back-quotes in Ruby.
 # `` => Ruby interpreter understand it as a system command.
@@ -11,9 +11,22 @@ pwd1 = `pwd`
 pwd2 = %x/pwd/
 
 # Two next will return the same result.
-puts(%(This is the directory where we are working now #{pwd1}))
-puts(%(This is the same result as the previous one: #{pwd2}))
+# puts(%(This is the directory where we are working now #{pwd1}))
+# puts(%(This is the same result as the previous one: #{pwd2}))
 # ---------------------------------------------------------------
 
 # Executing simple programms in my system.
 # x_my_browser = %x(min-browser &)
+# --------------------------------------------------------------
+
+# Concatenation
+hostname = %x/echo $HOSTNAME/
+greet1 = %(Hello ) "#{hostname.ljust(15, 'x')}" " 1st time."
+puts(greet1)
+
+# %x/command/ => Adds a new line.
+greet2 = "Hello " << "#{hostname}" << "2th time."
+puts(greet2.downcase)
+
+greet3 = %(Hello ) + %(#{hostname.reverse}) + " 3th time."
+puts(greet3.upcase)
