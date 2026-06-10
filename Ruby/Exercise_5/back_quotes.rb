a78a5be12804f6077c7f6e91a1a2de8a228f6271
DarioJr02 <dariocmia@gmail.com>
2026-06-10 11:30:00 +0200

    Exercise_5: x commands in Ruby

--- Ruby/Exercise_5/back_quotes.rb
+++ Ruby/Exercise_5/back_quotes.rb
@@ -3,11 +3,17 @@
 # This file is to practice back-quotes in Ruby.
 # `` => Ruby interpreter understand it as a system command.
 # %x/command/ => On LINUX
-# %x{command} => On WINDOWS
+# %x{command} => On WINDOWS and Linux
 
 # Embedding commands
 # puts(`pwd`) => Back-quotes.
 pwd1 = `pwd`
 pwd2 = %x/pwd/
+
+# Two next will return the same result.
 puts(%(This is the directory where we are working now #{pwd1}))
 puts(%(This is the same result as the previous one: #{pwd2}))
+# ---------------------------------------------------------------
+
+# Executing simple programms in my system.
+# x_my_browser = %x(min-browser &)
