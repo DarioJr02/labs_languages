7f6dea4352b0a281ba62c6c294ae6d4b2e3ba254
DarioJr02 <dariocmia@gmail.com>
2026-06-11 13:12:41 +0200

    Reversing range objects

--- Ruby/Exercise_5/ranges.rb
+++ Ruby/Exercise_5/ranges.rb
@@ -8,19 +8,20 @@ n2 = 20..35
 n3 = (-10..-5)
 n4 = ('a'..'z')
 
-
 # Using three-dot notation => Not include the last element.
 n1_tdt = (0...10)
 n2_tdt = (20...35)
-n3_tdt = (-5...-10)
+n3_tdt = (-10...-5)
 n4_tdt = ('a'...'z')
 
 # Reversing a range object?
+n5 = ('A'..'Z')
+n6 = (-20..-2)
 
 def it_with_a_range(obj)
   # We'll iterate within this range object with a for loop.
   for i in obj do
-    printf(%(#{i} ))
+    printf(%(#{i} ).chomp)
   end
   puts("\n")
 end
@@ -28,4 +29,13 @@ end
 it_with_a_range(n1)
 it_with_a_range(n2)
 it_with_a_range(n3)
-it_with_a_range(n4)
+it_with_a_range(n4.reverse_each)
+
+it_with_a_range(n1_tdt)
+it_with_a_range(n2_tdt)
+it_with_a_range(n3_tdt)
+it_with_a_range(n4_tdt)
+
+# Reversing ranges
+it_with_a_range(n5.reverse_each)
+it_with_a_range(n6.reverse_each)
