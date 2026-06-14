e4854d756ad9848259fa3f316fc0d2457a3d3069
DarioJr02 <dariocmia@gmail.com>
2026-06-14 09:42:00 +0200

    Exercise_5: Indexing into an Array

--- Ruby/Exercise_6/arrays.rb
+++ Ruby/Exercise_6/arrays.rb
@@ -11,3 +11,14 @@ print(%(#{arr_dq}\n))
 print("#{arr_sq.inspect}\n")
 print("#{arr_dq.inspect}\n")
 print(%(#{arr_imp.inspect}\n))
+
+# Indexing into an Array.
+print("#{arr_sq[-5..6]}\n")
+print("#{arr_dq[15].inspect}\n") # => Returns nil
+print("#{arr_dq[-1].inspect}\n")
+print(%(#{arr_dq.length}\n))
+print(%(#{arr_imp.length}\n))
+
+arr_dq.length == arr_imp.length ? puts(true) : puts(false)
+
+
