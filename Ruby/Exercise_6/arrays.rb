f0a879e152bca7252f5d63c513299abea825e060
DarioJr02 <dariocmia@gmail.com>
2026-06-14 09:17:47 +0200

    Exercise_6: Single and double quoted arrays

--- Ruby/Exercise_6/arrays.rb
+++ Ruby/Exercise_6/arrays.rb
@@ -2,8 +2,12 @@
 
 # This file is to practice arrays and some hashes.
 
-arr_sq = %w['one', 'two', 'three', 4, #{3 * 5}]
-arr_dq = %W['one', 'two', 'three', 4, #{3 * 5}]
+arr_sq = %w['one' 'two' 'three' 4 #{3 * 5}] # => Single-quoted array.
+arr_dq = %W['one' 'two' 'three' 4 #{3 * 5}] # => Double-quoted array.
+arr_imp = ['one', 'two', 'three', '4', (5 * 3)]
 
-puts(arr_sq)
-puts(arr_dq)
+print(%(#{arr_sq}\n))
+print(%(#{arr_dq}\n))
+print("#{arr_sq.inspect}\n")
+print("#{arr_dq.inspect}\n")
+print(%(#{arr_imp.inspect}\n))
