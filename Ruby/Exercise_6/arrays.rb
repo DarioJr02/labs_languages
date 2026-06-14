6c89f60bc80d2b976ed80d177ad4f8211d831ddc
DarioJr02 <dariocmia@gmail.com>
2026-06-14 08:41:07 +0200

    Arrays

---
+++ Ruby/Exercise_6/arrays.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# This file is to practice arrays and some hashes.
+
+arr_sq = %w['one', 'two', 'three', 4, #{3 * 5}]
+arr_dq = %W['one', 'two', 'three', 4, #{3 * 5}]
+
+puts(arr_sq)
+puts(arr_dq)
