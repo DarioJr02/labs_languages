132be9e031e3f7c5fb077f9760f4c5af167fbc72
DarioJr02 <dariocmia@gmail.com>
2026-06-15 12:34:46 +0200

    + array

--- Ruby/Exercise_6/arrays_commonly_methods.rb
+++ Ruby/Exercise_6/arrays_commonly_methods.rb
@@ -12,13 +12,32 @@ arr5 = [arr4[-1], :purple, :cian, arr4.index(:red)]
 def common_elements(array1, array2)
   equal_elements = (array1 & array2)
 
-  if (equal_elements.length == 0) then puts('NOT COMMONLY ITEM FOUND!') end
+  if (equal_elements.length == 0) then puts("NOT COMMONLY ITEM FOUND!\n\n") end
 
   for eq in equal_elements do # => Valid: for eq in (array1 & array2)
     puts(%(Commonly item: #{eq}))
   end
+  puts("\n")
 end
 
+# + => Returns an array concatenating 2 arrays.
+def concatenate(arr1, arr2)
+  arr_concatenated = (arr1 + arr2)
+
+  puts(%(arr_concatenated elements are:\n))
+
+  for element in arr_concatenated do
+    print(%(#{element}|))
+  end
+
+  puts("\n")
+end
+
+# & calls
 common_elements(arr1, arr2)
 common_elements(arr2, arr3)
 common_elements(arr4, arr5)
+
+# + calls
+concatenate(arr1, arr2)
+concatenate(arr3, arr4)
