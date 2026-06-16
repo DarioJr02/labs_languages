33e37246f3cf4d41b4648c83804dba34bff4f7f2
DarioJr02 <dariocmia@gmail.com>
2026-06-16 12:28:25 +0200

    Hash order are not evident

--- Ruby/Exercise_6/hashes.rb
+++ Ruby/Exercise_6/hashes.rb
@@ -7,7 +7,9 @@ h1['P1'] = 'Pedro'
 h1['P2'] = 'Carlos'
 h1['P3'] = 'Mendoza'
 h1['P4'] = 'Manny'
+h1.default = 'N\A Player'
 
+puts(%(Trying to call an undefined key-value: #{h1['P5']}))
 p(h1['p3'.capitalize])
 
 # Initializing a hash using shorthand:
@@ -15,6 +17,7 @@ h2 = { 'User1' => 'Carlos',
        'User2' => 'Mendoza',
        'User3' => 'Manny',
        'User4' => 'Carlos' }
+h2.default = 'N\A User'
 
 p(h2['USER1'.downcase.capitalize])
 
@@ -22,6 +25,7 @@ h3 = { 'City1' => 'Cundinamarca',
        'City2' => 'Rotterdam',
        'City3' => 'Paris',
        'City4' => 'Cali' }
+h3.default = 'N\A City'
 
 p(h3['city4'.capitalize])
 
@@ -30,9 +34,17 @@ id += 1
 
 h4 = { 'ID' => id += 1,
        'ID1' => id += 1 }
+h4.default = 'N\A ID'
 
 p(h4['ID'])
 p(h4['ID1'])
 
 # Accesing to a undefined key => return nil.
 p(h4['city1'])
+
+# Hash order in memory is not evident, but from Ruby 1.9 hashes stores in the same order that they're defined.
+print(%(\nHash order is not evident:\n))
+print("#{h1}\n\n")
+print("#{h2}\n\n")
+print("#{h3}\n\n")
+print("#{h4}\n\n")
