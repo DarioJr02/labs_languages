af45413b8129d563d82dea8c41a81611623e60e6
DarioJr02 <dariocmia@gmail.com>
2026-06-18 12:45:20 +0200

    while loops single line and multi-line

---
+++ Ruby/Exercise_7/while_loops.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# This file is to practice while loops.
+
+flag = true
+
+# puts('OK') while flag
+
+#while flag do 
+  #puts('OK')
+#end
+
+# puts(true) while 3.step(10) { |i| print("#{i} ") } < 10
+
+n = 0
+
+while n < 13
+  puts("n < 13 ?: #{flag}")
+  n = (n + 1)
+  print("#{n} ")
+
+  n >= 13 ? puts("n >= 13") : puts("n <= 13")
+end
+puts("\n")
