0e10fae1724385823348a78d1283e57850d89724
DarioJr02 <dariocmia@gmail.com>
2026-06-24 18:42:23 +0200

    Enumerable methods with hashes: .collect and min

--- Ruby/Exercise_7/enumerable_module.rb
+++ Ruby/Exercise_7/enumerable_module.rb
@@ -18,21 +18,21 @@ puts("\n2: .include?():")
 
 # Find a value in x
 puts('Tell me a value to find in x: ')
-value_to_find = gets.chomp
-x_p = x.include?(value_to_find.to_i)
-puts("x has the value: #{value_to_find}? -> #{x_p}")
+# value_to_find = gets.chomp
+# x_p = x.include?(value_to_find.to_i)
+# puts("x has the value: #{value_to_find}? -> #{x_p}")
 
 # Find a value in y
 puts("\nTell me a value to find in y: ")
-value_to_find_y = gets.chomp # => Jump line at the end is deleted with .chomp
-y_p = y.include?(value_to_find_y)
-puts("y has the value: #{value_to_find_y}? -> #{y_p}")
+# value_to_find_y = gets.chomp # => Jump line at the end is deleted with .chomp
+# y_p = y.include?(value_to_find_y)
+# puts("y has the value: #{value_to_find_y}? -> #{y_p}")
 
 # Find a value in z
 puts("\nTell me a value to find in z: ")
-v_to_find_z = gets.chomp
-z_p = z.include?(v_to_find_z)
-puts("z has the value: #{v_to_find_z}? -> #{z_p}")
+# v_to_find_z = gets.chomp
+# z_p = z.include?(v_to_find_z)
+# puts("z has the value: #{v_to_find_z}? -> #{z_p}")
 
 # .min
 print("\n3: .min:")
@@ -48,10 +48,22 @@ max value in y is -> #{y.max}\n
 max value in z is -> #{z.max}\n")
 
 # Enumerable methods with hashes
-print("\nEnumerable methods with hashes:")
+print("\nEnumerable methods with hashes:\n")
 
-h = { 'me' => 'c',
-      'you' => 'c++',
+h = { 'me' => 'c#',
+      'you' => 'c',
       'he' => 'go',
-      'her' => 'golang',
+      'her' => 'c++',
       'him' => 'rust' }
+puts(h)
+# .collect
+c = h.collect { |i| i }
+print("\nAfter apply .collect method:\n#{c}
+  \nWe can use some other methods like .flatten:\n#{c.flatten}\n")
+
+print("\nh min value is: #{h.min} and h max value is: #{h.max}\n")
+
+# comparing based on keys
+puts("Getting min value based on keys: #{h.min { |x, y| x[0].length <=> y[0].length }}")
+puts("Getting min based on values: #{h.min { |x, y| x[1].length <=> y[1].length }}")
+# comparing based on values
