d446080432bc09cf1ed04b7dfff05ba5d5e21c0c
DarioJr02 <dariocmia@gmail.com>
2026-06-27 11:57:41 +0200

    Ex8: Logical operators

--- Ruby/Exercise_8/conditionals_1.rb
+++ Ruby/Exercise_8/conditionals_1.rb
@@ -2,7 +2,7 @@
 
 # The purpose of this file is to practice conditionals: if/then/else
 
-# Multiline conditional sentences
+# 1. Multiline conditional sentences
 
 day_weather = 'Raining'
 
@@ -17,6 +17,19 @@ end
 day_weather = 'sunny'.capitalize
 
 puts('Use some sun cream.') if day_weather == 'Sunny'
-
 if day_weather == 'Sunny' then puts('Use some sun cream.') end
-# if day_weather == 'Sunny' : puts('Use some sun cream.') # Only works in Ruby 1.8
+# if day_weather == 'Sunny' : puts('Use some sun cream.') # Works in Ruby 1.8 only.
+
+# 2. Operators and precedence.
+
+# - Logical Operators => and/or/not, &&/||/!
+
+a_day = 'Saturday'
+working_overtime = true
+
+
+
+puts('Hurrah!') if a_day == 'Saturday' or a_day == 'Sunday' and not working_overtime # it will never be executed.
+puts('Hurrah!') if a_day == 'Saturday' or (a_day == 'Sunday' and not working_overtime)
+
+puts('Hurrah! (Using &&/||/!).') if a_day == 'Saturday' || a_day = 'Saturday' && !working_overtime
