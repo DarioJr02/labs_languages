4115d99711342a92c6f808120c6d87711bb03c9a
DarioJr02 <dariocmia@gmail.com>
2026-06-27 13:56:08 +0200

    Case equality operator ===

--- Ruby/Exercise_8/conditionals_1.rb
+++ Ruby/Exercise_8/conditionals_1.rb
@@ -29,7 +29,19 @@ working_overtime = true
 
 
 
-puts('Hurrah!') if a_day == 'Saturday' or a_day == 'Sunday' and not working_overtime # it will never be executed.
+puts('Hurrah!') if a_day == 'Saturday' or a_day == 'Sunday' and not working_overtime # and has a higher precedence than or.
 puts('Hurrah!') if a_day == 'Saturday' or (a_day == 'Sunday' and not working_overtime)
 
 puts('Hurrah! (Using &&/||/!).') if a_day == 'Saturday' || a_day = 'Saturday' && !working_overtime
+
+# 3. === Case equality operator => It returns a boolean value.
+
+# checking object class type.
+puts(String === 's')
+puts(Integer === 3.4.to_int)
+puts(Float === 3.4)
+puts(Range === ('b'..'h'))
+puts(Range === ('b'..'h').to_a)
+puts(Hash === {'Victor' => 9})
+
+puts(('a'..'r') === 'k') # 
