996e12252f8e1daef3d1640f15753c3a1ec05e82
DarioJr02 <dariocmia@gmail.com>
2026-06-29 10:29:49 +0200

    when x

--- Ruby/Exercise_8/case_statements.rb
+++ Ruby/Exercise_8/case_statements.rb
@@ -13,6 +13,12 @@ def print_day(day)
 
   case day
   when 1 then puts("It's Monday.")
+  when 2 then puts("It's Thursday.")
+  when 3 then puts("It's Wednesday.")
+  when 4 then puts("It's Tuesday.")
+  when 5 then puts("It's Friday.")
+  when 6, 7 then puts("It's Weekend.")
+  else puts("That's not a real day.")
   end
 end
 
