ea6975ccb81bf010b83eb879f9bb1dc0d7b94419
DarioJr02 <dariocmia@gmail.com>
2026-06-30 12:00:30 +0200

    Ex9: Docs

--- Ruby/Exercise_9/c_methods_utilities.rb
+++ Ruby/Exercise_9/c_methods_utilities.rb
@@ -6,10 +6,10 @@ fn = 'iMethods_cMethods.rb'
 
 if File.exist?(fn)
   puts('It does exist.')
-  puts("File path: #{File.expand_path(fn)}\n",
-       "File basename: #{File.basename(fn)}\n",
-       "File dirname: #{File.dirname(fn)}",
-       "File extension: #{File.extname(fn)}",
+  puts("File path: #{File.expand_path(fn)}\n", # => File full path based on user's home directory.
+       "File basename: #{File.basename(fn)}\n", # => File's name.
+       "File dirname: #{File.dirname(fn)}", # => directory name based un relative working directory.
+       "File extension: #{File.extname(fn)}", # => File's extension.
        "File mtime: #{File.mtime(fn)}", # => Timestamp when the file was created.
-       "File size in bytes: #{File.size(fn)}")
+       "File size in bytes: #{File.size(fn)}") # => File size in bytes.
 end
