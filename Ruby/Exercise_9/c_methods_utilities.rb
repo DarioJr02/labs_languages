8256fc4bd6b7d8e9d1d9e9412f9a5b826de9bf65
DarioJr02 <dariocmia@gmail.com>
2026-06-30 11:56:45 +0200

    File class methods

--- Ruby/Exercise_9/c_methods_utilities.rb
+++ Ruby/Exercise_9/c_methods_utilities.rb
@@ -6,7 +6,10 @@ fn = 'iMethods_cMethods.rb'
 
 if File.exist?(fn)
   puts('It does exist.')
-  puts("File path: #{File.expand_path(fn)}")
-  puts("File basename: #{File.basename(fn)}")
-  puts("")
+  puts("File path: #{File.expand_path(fn)}\n",
+       "File basename: #{File.basename(fn)}\n",
+       "File dirname: #{File.dirname(fn)}",
+       "File extension: #{File.extname(fn)}",
+       "File mtime: #{File.mtime(fn)}", # => Timestamp when the file was created.
+       "File size in bytes: #{File.size(fn)}")
 end
