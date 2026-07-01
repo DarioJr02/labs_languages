76b61422a7d169944e2d85b3076ab02c39e38c1e
DarioJr02 <dariocmia@gmail.com>
2026-07-01 17:48:41 +0200

    Ex9: Refact

--- Ruby/Exercise_9/singleton_methods.rb
+++ Ruby/Exercise_9/singleton_methods.rb
@@ -15,4 +15,14 @@ puts("AThing.class => #{AThing.class}",
      "Array.class => #{Array.class}",
      "Range.class => #{Range.class}",
      "Hash.class => #{Hash.class}",
-     "Class.class => #{Class.class}")
+     "Class.class => #{Class.class}\n\n")
+
+# Print Singleton Methods.
+def print_singleton_methods(class_name, class_object)
+  puts("#{class_name} Singleton Methods => #{class_object.singleton_methods}")
+end
+
+print_singleton_methods('IO', IO)
+print_singleton_methods('Class', Class)
+print_singleton_methods('String', String)
+print_singleton_methods('TrueClass', TrueClass)
