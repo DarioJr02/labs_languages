f1beecd7f3205a08a271887428b06ce19b8e9d4f
DarioJr02 <dariocmia@gmail.com>
2026-07-03 12:48:18 +0200

    Ex9: .define_singleton_method

--- Ruby/Exercise_9/singleton_classes.rb
+++ Ruby/Exercise_9/singleton_classes.rb
@@ -14,7 +14,7 @@ class Profession
 end
 
 # 2. Profession class instanced objects.
-puts("Profession class instanced object:")
+puts("1. Profession class instanced object:")
 baseball_player = Profession.new('baseball_player', 'at bat every day.')
 puts(baseball_player.do_something)
 
@@ -27,15 +27,18 @@ puts(baseball_player.play_another_position)
 puts("Does Profession Class have any singleton method? ==> #{Profession.singleton_methods.any?}")
 puts("Is Profession.singleton_methods Array empty? ==> #{Profession.singleton_methods.empty?}")
 
+# -------------------------------------------------
 # 3. Opening baseball_player eigen/singleton class.
-puts("\nOpening baseball_player singleton class:")
+puts("\n2. Opening baseball_player singleton class:")
 
 class << baseball_player
   def studying_to_be_a_manager
     puts("I'm studying to be a baseball manager.")
   end
 end
+# -------------------------------------------------
 
+# -------------------------------------------------------------------
 if baseball_player.singleton_methods.include?(:studying_to_be_a_manager)
   baseball_player.studying_to_be_a_manager
 else
@@ -53,3 +56,13 @@ if baseball_player.class.method_defined?('studying_to_be_a_manager')
 else
   puts("Method not defined for #{baseball_player.class}.")
 end
+# -------------------------------------------------------------------
+
+puts("\n3. Define a singleton method using ==> .define_singleton_method:")
+# Another way to do it, used in metaprogramming.
+baseball_player.define_singleton_method(:doing_stuff) do
+  print("I'm doing some stuff.")
+end
+
+puts("is doing_stuff method defined in baseball_player? ==> #{baseball_player.respond_to?(:doing_stuff)}")
+puts(baseball_player.doing_stuff)
