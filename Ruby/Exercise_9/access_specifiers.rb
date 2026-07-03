6a0350ac72776d3b3e731280158e4878e4cf5c08
DarioJr02 <dariocmia@gmail.com>
2026-07-03 17:46:07 +0200

    Ex9: public and private access specifiers

--- Ruby/Exercise_9/access_specifiers.rb
+++ Ruby/Exercise_9/access_specifiers.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
 # The goal of this file is to learn how access specifiers work.
+
+# 1. Profession Class.
+class Profession
+  public # => is initialize public by default?
+  def initialize(profession, common_task)
+    @profession = { profession => common_task }
+  end
+
+  # do_task
+  def do_task
+    print("My most common task as a #{@profession.keys} is #{@profession.values}")
+  end
+
+  def rest
+    print(sleep)
+  end
+
+  # private mehtods
+  private
+
+  def sleep
+    return "I need to rest to do my work tomorrow."
+  end
+end
+
+football_player = Profession.new('Football player', 'running.')
+
+puts(football_player.do_task)
+puts(football_player.rest)
