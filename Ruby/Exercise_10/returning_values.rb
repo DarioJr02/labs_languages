30891baf9a43d968e4af952de71935cf420e45f7
DarioJr02 <dariocmia@gmail.com>
2026-07-08 11:59:36 +0200

    Ex10: Return values

--- Ruby/Exercise_10/returning_values.rb
+++ Ruby/Exercise_10/returning_values.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
 # The goal of this file is to learn and practice about:
-# - Returning values.
+# - Returning values, multiple returning values.
 # - Method args.
+
+# This class is to practice the returning value concept.
+class Item
+  def initialize(arg1, arg2)
+    @arg1 = arg1
+    @arg2 = arg2
+  end
+
+  # Singleton methods
+  class << self
+    # 1. Returns: nil.
+    def r1() end
+
+    # 2. Returns: int or float
+    def r2(arg)
+      return ((arg**2 * -1)).abs
+    end
+  end
+end
+
+puts('1. Returning values:')
+ob = Item.new('arrow', 'bottle')
+
+puts("The return type of r1 method is: NilClass? ==> #{Item.r1.nil?}")
+puts("The return type of r2 is ==> #{Item.r2(3)}")
