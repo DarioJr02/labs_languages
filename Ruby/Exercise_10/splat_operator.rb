a7853060e498a53ca20ab6e939eb856017cfd17d
DarioJr02 <dariocmia@gmail.com>
2026-07-09 12:17:25 +0200

    Ex10: Splat opeartor *

--- Ruby/Exercise_10/splat_operator.rb
+++ Ruby/Exercise_10/splat_operator.rb
@@ -3,3 +3,14 @@
 # The goal of this file is to practice ->
 # - Splat operator (*arg)
 # - Argument priority in method sign.
+
+# 1. splat opeartor (*arg)
+
+def splat_op(one, two, three, *four)
+  return [one, two, three, four]
+end
+
+ob1 = splat_op(1, 2, 3, '4', 'four', 4.to_f)
+puts("ob1 content ==> #{ob1}")
+
+# - Another method sign.
