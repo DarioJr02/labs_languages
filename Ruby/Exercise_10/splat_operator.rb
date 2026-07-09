ae27ab5bd4d8018c0ebc3d602e07f19568c1a2e4
DarioJr02 <dariocmia@gmail.com>
2026-07-09 12:48:42 +0200

    Ex10: Splat arg order resolution

--- Ruby/Exercise_10/splat_operator.rb
+++ Ruby/Exercise_10/splat_operator.rb
@@ -22,10 +22,15 @@ end
 ob2 = splat_op2(1, 2, 3, 4, 5, 6, 7, 8)
 puts("ob2 content ==> #{ob2}")
 
-# ???
+# - Mandatory params resolvs first.
 def splat_op3(one, two, *three, four)
   return [one, two, three, four]
 end
 
 ob3 = splat_op3(1, 2, 3, 4, 5, 6, 7)
 puts("ob3 content ==> #{ob3}")
+
+# => Unexpected * splat parameters.
+# def splat_op4(one, *two, three, *four)
+  # return [one, two, three, four]
+# end
