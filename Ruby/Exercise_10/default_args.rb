6895be92714d2dfbaa8a5ed7a25066ecfc583586
DarioJr02 <dariocmia@gmail.com>
2026-07-09 11:12:45 +0200

    Ex10: Default args

--- Ruby/Exercise_10/default_args.rb
+++ Ruby/Exercise_10/default_args.rb
@@ -4,6 +4,18 @@
 # - Default and multiple arguments.
 
 # 1. Methods to practice default arguments.
+def data(name = 'Uknown', id = 0)
+  return {'name' => name, 'ID' => id }
+end
+
+h1 = data
+puts("h1 key and value ==> #{h1}")
+
+h2 = data('Matt', 3)
+puts("h2 key and value ==> #{h2}")
+
+# h3 = data('Karl', 8, 'value') # ==> Error, expected 2 args, 3 provided.
+# puts("h3 key and value ==> #{h3}")
 
 # 2. Class to pratice default arguments.
 class Phone
