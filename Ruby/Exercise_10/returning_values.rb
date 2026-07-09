6a936fefaebafa0639dd29f5918e1c1622eb7783
DarioJr02 <dariocmia@gmail.com>
2026-07-09 10:33:10 +0200

    Ex10: Returning multiple values

--- Ruby/Exercise_10/returning_values.rb
+++ Ruby/Exercise_10/returning_values.rb
@@ -16,24 +16,31 @@ class Item
     # 1. Returns: nil.
     def r1() end
 
-    # 2. Returns: int or float
+    # 2. Implicit return.
     def r2(arg)
-      (arg**2 * -1).abs # Return implicitly.
+      (arg**2 * -1).abs
     end
 
-    # 3. Returns: Explicitly return statement finishes the method inmediately.
+    # 3. Explicit return, ends the method inmediately.
     def r3(arg)
       return (arg**3 * -1).to_f
+    end
+
+    # 4. Implicit return of multiple values.
+    def r4_mult_values(arg1, arg2, arg3)
+      # arg1, arg2, arg3 # => Error.
+
+      [arg1 * 1, arg2 * 2, arg3 * 3] # => Explicit Array, OK.
+      # return [arg1, arg2, arg3] # => OK.
     end
   end
 end
 
 puts('1. Returning values:')
-ob = Item.new('arrow', 'bottle')
 
-puts("The return type of r1 method is: NilClass? ==> #{Item.r1.nil?}")
-puts("The return type of r2 is ==> #{Item.r2(3)} and its type ==> #{Item.r2(3).class}")
+puts("Return type of r1 method is: NilClass? ==> #{Item.r1.nil?}")
+puts("Return value of r2 ==> #{Item.r2(3)} and its type ==> #{Item.r2(3).class}")
+puts("Return value of r2 ==> #{Item.r2(3.59)} and its type ==> #{Item.r2(3.59).class}")
 
-# Por qué aquí si pongo 3.59 me devuleve int?
-puts("The return type of r2 is ==> #{Item.r2(3.59)} and its type ==> #{Item.r2(3.59).class}")
-puts("The return type of r3 is ==> #{Item.r3(9.3)} and its type ==> #{Item.r3(3.59).class}")
+puts("Return value of r3 ==> #{Item.r3(9.3)} and its type ==> #{Item.r3(9.3).class}")
+puts("Return value of r4 ==> #{Item.r4_mult_values(8, 9, 10)} and its type ==> #{Item.r4_mult_values(8, 9, 10).class}")
