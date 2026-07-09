0ebd391fa56b37c3dc1d073956f477149d608c5f
DarioJr02 <dariocmia@gmail.com>
2026-07-09 12:30:54 +0200

    Ex10: Arguments resolution order

--- Ruby/Exercise_10/splat_operator.rb
+++ Ruby/Exercise_10/splat_operator.rb
@@ -6,11 +6,26 @@
 
 # 1. splat opeartor (*arg)
 
-def splat_op(one, two, three, *four)
+def splat_op1(one, two, three, *four)
   return [one, two, three, four]
 end
 
-ob1 = splat_op(1, 2, 3, '4', 'four', 4.to_f)
+ob1 = splat_op1(1, 2, 3, '4', 'four', 4.to_f)
 puts("ob1 content ==> #{ob1}")
 
-# - Another method sign.
+
+# - Right-left resolution.
+def splat_op2(*one, two, three, four)
+  return [one, two, three, four]
+end
+
+ob2 = splat_op2(1, 2, 3, 4, 5, 6, 7, 8)
+puts("ob2 content ==> #{ob2}")
+
+# ???
+def splat_op3(one, two, *three, four)
+  return [one, two, three, four]
+end
+
+ob3 = splat_op3(1, 2, 3, 4, 5, 6, 7)
+puts("ob3 content ==> #{ob3}")
