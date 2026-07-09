6695ec804c7f6d9053349cc29c854e837add03bf
DarioJr02 <dariocmia@gmail.com>
2026-07-09 18:02:16 +0200

    Ex10: = operator mutability

--- Ruby/Exercise_10/pass_by_value_of_reference.rb
+++ Ruby/Exercise_10/pass_by_value_of_reference.rb
@@ -3,15 +3,35 @@
 # The goal of this file is understand and practice ->
 # - Pass-by-value-of-reference.
 
-def pass(name)
-  name = 'Jessica'
+def pass(name, new_name)
+  name = new_name
 
-  puts("At this method i'm #{name.equal?('Mark') ? 'Mark' : 'Jessica'}.")
+  msg = "Now i'm #{name} | Object id ==> #{name.object_id}"
+  puts("AT METHOD PASS ==> #{msg}")
+
+  return msg
 end
 
+# Mark
 n = 'Mark'
-puts("My name is #{n}.")
+puts("#{n}:\n")
+puts("My name is #{n} | Object id ==> #{n.object_id}")
 
-pass(n)
+n_id = n.object_id
 
-puts("My name is Mark? ==> #{n.equal?('Mark')}")
+pass(n, 'Jesus')
+puts("Is my name Mark? ==> #{n.eql?('Mark') ? 'Yes' : "No, my name is #{n}"}.")
+puts("Is my id still #{n_id}? ==> #{n.object_id.eql?(n_id)}")
+
+# Carlos
+n2 = 'Carlos'
+puts("\n#{n2}:\n")
+
+puts("My name is #{n2} | Object id ==> #{n2.object_id}")
+
+n2_id = n2.object_id
+
+pass(n2, 'Miguel')
+puts("Is my name #{n2}? ==> #{n2.eql?('Carlos') ? 'Yes' : "No, my name is #{n2}"}")
+puts("Is my id still #{n2_id}? ==> #{n2.object_id.eql?(n2_id)}")
+
