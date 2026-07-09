f75445085a9818409458cacd56ccc96bcc11d733
DarioJr02 <dariocmia@gmail.com>
2026-07-09 10:57:04 +0200

    Ex10: default_args.rb

---
+++ Ruby/Exercise_10/default_args.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# The goal of this file is to practice ->
+# - Default and multiple arguments.
+
+# 1. Methods to practice default arguments.
+
+# 2. Class to pratice default arguments.
+class Phone
+end
