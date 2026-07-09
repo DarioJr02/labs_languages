5a8ec8aeb2c195ebff7a79a955543c6d8cc32cc8
DarioJr02 <dariocmia@gmail.com>
2026-07-09 17:30:33 +0200

    Ex10: Pass-by-value-of-reference

--- Ruby/Exercise_10/pass_by_value_of_reference.rb
+++ Ruby/Exercise_10/pass_by_value_of_reference.rb
@@ -2,3 +2,16 @@
 
 # The goal of this file is understand and practice ->
 # - Pass-by-value-of-reference.
+
+def pass(name)
+  name = 'Jessica'
+
+  puts("At this method i'm #{name.equal?('Mark') ? 'Mark' : 'Jessica'}.")
+end
+
+n = 'Mark'
+puts("My name is #{n}.")
+
+pass(n)
+
+puts("My name is Mark? ==> #{n.equal?('Mark')}")
