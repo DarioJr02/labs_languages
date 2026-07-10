fee7efa0cabdf5b623a31c0bc4df2d77cae40d9f
DarioJr02 <dariocmia@gmail.com>
2026-07-10 13:40:42 +0200

    Ex10: Values mutability

--- Ruby/Exercise_10/string_mutability.rb
+++ Ruby/Exercise_10/string_mutability.rb
@@ -1,19 +1,32 @@
 ## frozen_string_literal: true
 
 # The goal of this file is to understand ->
-# String mutability.
+# - Values mutability.
+# - Mutable Methods and Operators: <<, method!.
+
+# 1. Values mutability.
+puts('1. Values mutability:')
 
 name1 = 'John'
 name2 = 'John'
 
-puts('Checking object.id:')
-puts("name1 ID ==> #{name1.object_id}")
-puts("name2 ID ==> #{name2.object_id}")
+name1_id = name1.object_id
+name2_id = name2.object_id
+
+puts("name1 ID ==> #{name1_id}")
+puts("name2 ID ==> #{name2_id}")
 
 name2 = 'Jessica'
 puts("name2 ID ==> #{name2.object_id}")
 
-100.times do
-  name3 = 'Carl'
-  puts("nam3 ID ==> #{name3.object_id}")
-end
+# 2. Mutable operators:
+puts("\n2. Mutable opeartors:")
+
+name1 << ' Doe'
+name2 << ' McNeal'
+
+puts("name 1 value now is ==> #{name1}.
+  Is its ID the same? ==> #{name1.object_id.equal?(name1_id) ? "Yes, it is." : "No, it's #{name1.object_id}"}")
+
+puts("name 2 value now is ==> #{name2}.
+Is its ID the same? ==> #{name2.object_id.equal?(name2_id) ? 'Yes, it is.' : "No, it's #{name2.object_id}"}")
