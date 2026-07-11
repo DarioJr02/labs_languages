b3cd404c7732487a53d7912c4c14d701dab01ab2
DarioJr02 <dariocmia@gmail.com>
2026-07-11 17:37:36 +0200

    Ex10: Objects ID

--- Ruby/Exercise_10/objects.rb
+++ Ruby/Exercise_10/objects.rb
@@ -2,3 +2,15 @@
 
 # The goal of this file is to practice about ->
 # - Ruby design philosophy about objects.
+
+def get_object_id(*obj)
+  arr = []
+  arr.push(*obj)
+
+  arr.each { |i| puts("ID of this object ==> #{i.object_id}") }
+end
+
+obj1 = 20
+get_object_id(1, '1', 2, '2', 3, '3')
+
+# get_object_id(1.to_s, '1', 2.to_s, '2', 3.to_s, '3')
