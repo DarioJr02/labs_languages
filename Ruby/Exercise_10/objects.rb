0e2839367514c3dca48bc27501328bbe82ef0368
DarioJr02 <dariocmia@gmail.com>
2026-07-12 18:41:42 +0200

    Ex10: Canonical values ==> Integer

--- Ruby/Exercise_10/objects.rb
+++ Ruby/Exercise_10/objects.rb
@@ -1,4 +1,4 @@
-# #frozen_string_literal: true
+# frozen_string_literal: true
 
 # The goal of this file is to practice about ->
 # - Ruby design philosophy about objects.
@@ -10,42 +10,34 @@ def get_object_id(*obj)
   arr.each { |i| puts("ID of this object ==> #{i.object_id}") }
 end
 
-obj1 = 20
-get_object_id(1, '1', 2, '2', 3, '3', 3)
+# get_object_id(1, '1', 2, '2', 3, '3', 3)
 
 # get_object_id(1.to_s, '1', 2.to_s, '2', 3.to_s, '3')
 
+# 1. Check if a value is canonical.
+
 def compare_id(obj1, obj2)
-  puts(obj1.object_id.equal?(obj2.object_id),
-       obj1.object_id.equal?(obj2.object_id) ? "#{obj1} is a canonical value." : "It's not a canonical value.")
+  puts(obj1.object_id.equal?(obj2.object_id) ? "#{obj1} is a canonical value | Type = #{obj1.class}" : "#{obj1.class} it's not a canonical value.")
 end
 
-n = 10
-n2 = 10
-compare_id(n, n2)
+# - Integer
+compare_id(10, 10)
+compare_id(Integer(10), Integer(10))
+compare_id('10'.to_i, '10'.to_i)
+compare_id((100.0 / 10.0).to_i, (100 / 10))
 
-s1 = 'hey'
-s2 = 'hey'
-compare_id(s1, s2)
+# - FLoat
 
-f1 = 3.10
-f2 = 3.10
-compare_id(f1, f2)
+# - String
 
-h1 = { 'ID' => 11 }
-h2 = { 'ID' => 11 }
-compare_id(h1, h2)
+# - Array
 
-t = true
-f = true
-compare_id(t, f)
+# - Hash
 
-sym1 = :a
-sym2 = :a
-compare_id(sym1, sym2)
+# - True
 
-nil1 = nil
-nil2 = nil
-compare_id(nil1, nil2)
+# - False
 
-compare_id(Float(1.1), Float(1.1))
+# - nil
+
+# - Symbol
