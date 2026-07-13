50846a5f9a7ed32006971baae4b290224e19ca5f
DarioJr02 <dariocmia@gmail.com>
2026-07-13 18:14:07 +0200

    Ex10: Multiple variable assignment with ranges and arrays

--- Ruby/Exercise_10/x_multiple_assignment.rb
+++ Ruby/Exercise_10/x_multiple_assignment.rb
@@ -16,3 +16,10 @@ print("As an Array ==> #{arr}")
 
 a2, b2, c2, d2, e = box('Plush', 'T-Rex', 'Candies pack', 'WWE Card')
 puts("\n#{e.nil?}")
+
+a_g, h_k, l_p, q_z = ('a'..'g').to_a, ('h'..'k').to_a, ('l'..'p').to_a, ('q'..'z').to_a
+puts("Alphabet:
+a..g=#{a_g}
+h..k=#{h_k}
+l..p=#{l_p}
+q..z=#{q_z}")
