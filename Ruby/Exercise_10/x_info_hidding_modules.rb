52650280a61c6cded96425b931b32060ba4af56d
DarioJr02 <dariocmia@gmail.com>
2026-07-13 11:10:08 +0200

    Ex10: Information hidding ==> Modules

--- Ruby/Exercise_10/x_info_hidding_modules.rb
+++ Ruby/Exercise_10/x_info_hidding_modules.rb
@@ -2,3 +2,26 @@
 
 # The goal of this file is to learn and practice ->
 # - Information hidding using modules.
+
+module Farm
+  class Chicken
+  end
+
+  class Cow
+  end
+
+  class Goat
+  end
+end
+
+# ob = Chicken.new # ==> Error: uninitialized constant Chicken.
+# puts(ob.class)
+
+ob = Farm::Chicken.new
+puts(ob, ob.class)
+
+ob2 = Farm::Cow.new
+puts(ob, ob2.class)
+
+ob3 = Farm::Goat.new
+puts(ob3, ob3.class)
