b41381b5a51df0f33af7621ee5aba0baaf4aaf02
DarioJr02 <dariocmia@gmail.com>
2026-07-13 18:09:09 +0200

    Ex10: Multiple variable assignment ==> extra var as nil

--- Ruby/Exercise_10/x_multiple_assignment.rb
+++ Ruby/Exercise_10/x_multiple_assignment.rb
@@ -9,5 +9,10 @@ def box(obj1, obj2, obj3, obj4)
 end
 
 a, b, c, d = box('Plush', 'T-Rex', 'Candies pack', 'WWE Card')
+puts("a=#{a}, b=#{b}, c=#{c}, d=#{d}")
 
-puts(a)
+arr = box('Plush', 'T-Rex', 'Candies pack', 'WWE Card')
+print("As an Array ==> #{arr}")
+
+a2, b2, c2, d2, e = box('Plush', 'T-Rex', 'Candies pack', 'WWE Card')
+puts("\n#{e.nil?}")
