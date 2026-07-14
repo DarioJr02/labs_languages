18ddfd135be4c17cc0876590dc1ae01dc762b35b
DarioJr02 <dariocmia@gmail.com>
2026-07-14 10:38:01 +0200

    Ex11: ZeroDivisionError

--- Ruby/Exercise_11/x_exceptions.rb
+++ Ruby/Exercise_11/x_exceptions.rb
@@ -2,3 +2,11 @@
 
 # The goal of this file is to loearn and practice about ->
 # - Exceptions.
+
+# Here's a good point to learn how to debug code.
+
+def method_unhandled(x)
+  return (x/0)
+end
+
+x = method_unhandled(2)
