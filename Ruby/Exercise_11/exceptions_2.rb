c8c9f0157a8b4033e72c3b9d508a631da82a2b74
DarioJr02 <dariocmia@gmail.com>
2026-07-29 18:59:22 +0200

    Ex11: Exceptions, backtrace

--- Ruby/Exercise_11/exceptions_2.rb
+++ Ruby/Exercise_11/exceptions_2.rb
@@ -12,18 +12,22 @@ module Exceptions # rubocop:disable Style/Documentation
   end
 
   # 2. Exception with e.message.
+  # Give me two values.
   def self.calc(val1, val2)
     return (val1 / val2)
   rescue StandardError => e
-    puts(e.message)
+    puts("Exception Message: [#{e.message}]
+Exception Type: [#{e.class}]
+Exception Backtrace: [#{e.backtrace}]")
   end
 end
 
-puts('1. e.message:')
+puts('1. e methods:')
 x = Exceptions.calc(3, 5)
 puts(x)
 
 y = Exceptions.calc(91, 2)
 puts(y)
 
-puts('2. Exception with e.backtrace.')
+z = Exceptions.calc('0', 'a')
+puts(z)
