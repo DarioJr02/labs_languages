15dd51a9a298917377647f4fffb491fcc5afc4a9
DarioJr02 <dariocmia@gmail.com>
2026-07-31 11:14:13 +0200

    Ex11: Modern way to apply retry concept

--- Ruby/Exercise_11/exceptions_5_retry.rb
+++ Ruby/Exercise_11/exceptions_5_retry.rb
@@ -30,5 +30,35 @@ class Wallet
   end
 end
 
-w = Wallet.new(-1)
-puts(w.balance)
+# w = Wallet.new(-1)
+# puts(w.balance)
+
+# Wallet2 class:
+class Wallet2
+  attr_accessor(:balance)
+
+  # 1. Constructor:
+  def initialize(balance)
+    raise CustomExceptions::NegativeBalance, 'Balance cannot be negative.' if balance.negative?
+
+    @balance = balance
+
+    attempt = 0
+    while attempt < 3
+      attempt += 1
+
+      begin
+        break
+      rescue StandardError => e
+        puts("#{e.message}
+        #{e.class}
+        #{e.backtrace}")
+
+        raise if attempt < 3
+      end
+    end
+  end
+end
+
+w2 = Wallet2.new(-1)
+puts(w2.balance)
