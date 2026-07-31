afdcfdf750bbb2a672b60d0e03d786831bdbc0a9
DarioJr02 <dariocmia@gmail.com>
2026-07-31 09:49:57 +0200

    Ex11: Custom exceptions with raise

---
+++ Ruby/Exercise_11/exceptions_4_raise.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# At this file we'll learn about raise exceptions and custom exceptions with inheritance.
+
+class InsufficientBalance < StandardError
+end
+
+class InvalidUserName < StandardError
+end
+
+# Customer class
+class Customer
+  # 0. accessors:
+  attr_accessor(:_user_name, :_balance)
+
+  # 1. Constructor:
+  def initialize(user_name, balance)
+    @_user_name = user_name
+    @_balance = balance
+  end
+end
+
+# Custom Exceptions
+module CustomExceptions
+  def self.item_payment(customer)
+    price = 300
+    raise InsufficientBalance, 'Customer Balance Insufficient.' if customer._balance < price
+
+    customer._balance -= price
+  rescue ArgumentError => e
+    puts("#{e.message}
+    #{e.class}
+    #{e.backtrace}")
+
+  end
+end
+
+c1 = Customer.new('Toño', 459)
+puts("Customer's user name: #{c1._user_name} | Balance #{c1._balance}")
+
+CustomExceptions.item_payment(c1)
+puts(c1._balance)
