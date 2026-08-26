f3fae5df9b078217190c6c851749a0c45db50f28
DarioJr02 <dariocmia@gmail.com>
2026-08-26 18:58:46 +0200

    Block parameters arity

--- Ruby/Exercise_12/procs_1.rb
+++ Ruby/Exercise_12/procs_1.rb
@@ -29,11 +29,37 @@ my_proc2 = proc { print("This is my second #{self.class}, who contains a block."
 
 def p_method_2(p0, p1, &block)
   puts("Using my second #{block.class} inside a method:")
-  block.call(p0, p1, block) if block_given?
+  block.call(p0, p1) if block_given?
 end
 
 p_method_2(a1, a2) do |x, y|
   x.each { |n| print("#{n * 2} ") }
   puts("\n")
   y.each { |n| print("#{n * 3} ") }
+end
+
+# Testing block parameters:
+def p_method_3(p0, p1, &block)
+  puts("Using my third #{block.class} inside: p_method_3:")
+  block.call(p0, p1) if block_given?
+end
+
+p_method_3(a1, a2) do |x, y, z, a, b, c|
+  x.each { |n| print("#{n * 4} ") }
+  puts("\n")
+  y.each { |n| print("#{n * 5} ") }
+  puts("\n#{z.class}")
+  puts(a.class)
+  puts(b.class)
+  puts(c.class)
+end
+
+def p_method_4(p0, p1, &block)
+  puts("Using my fourth #{block.class} inside: p_method_4:")
+  block.call(p0, p1, 3, 4, 5) if block_given?
+end
+
+p_method_4(a1, a2) do |x, y, z|
+  puts(x.class)
+  puts(y.class)
 end
