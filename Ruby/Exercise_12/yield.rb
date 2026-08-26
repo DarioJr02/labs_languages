c7d133a7d62a2850f6e266b49f4ab34cddf5cc8a
DarioJr02 <dariocmia@gmail.com>
2026-08-26 12:15:22 +0200

    Using method parameters as block parameters

--- Ruby/Exercise_12/yield.rb
+++ Ruby/Exercise_12/yield.rb
@@ -3,6 +3,8 @@
 # At this file we'll learn about ==>
 # yield
 # block_given?
+# capture block as a parameter.
+# using method parameters as block parameters.
 
 # 1: yield
 def a_method
@@ -35,8 +37,54 @@ a_method_bg do
   puts('This is a block inside a_method_bg')
 end
 
-def test
-  yield
+# Summary
+
+def y_method
+  puts('Starting: y_method.')
+  yield if block_given?
+  puts('Ending: y_method.')
 end
 
-test
+y_method do
+  puts((0..2).to_a)
+end
+
+def y_method_h
+  puts('Starting: y_method.')
+  yield if block_given?
+  puts('Ending: y_method.')
+end
+
+3.times do
+  y_method_h do
+    puts('block')
+  end
+end
+
+# Capture block as a parameter
+puts('Capture block as a parameter:')
+
+def bp_method(&block)
+  puts('Starting: bp_method.')
+  block.call if block_given?
+  puts('Ending: bp_method.')
+end
+
+bp_method do
+  puts('Block as parameter.')
+end
+
+# Using method parameter as a block parameter.
+p = { 'Name' => 'Pepe', 'Age' => 39 }
+puts("\nUsing method parameter as a block parameter:\n")
+
+def bp_method2(p, &block)
+  puts('Starting: bp_method2.')
+  block.call(p) if block_given?
+  puts('Ending: bp_method2.')
+  ref = block
+end
+
+bp_method2(p) do |p|
+  p.each { |key, value| puts("#{key}: #{value}") }
+end
